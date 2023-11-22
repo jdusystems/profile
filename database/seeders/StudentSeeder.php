9172e0d0ab623c08f48002cc2a0c8758e80ec2e5
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2023-11-22 16:03:02 +0500

    StudentSeeder has been done

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $studentsData = [
+            ['student_id' => "211132" ,'surname' => "Abduvohidov",'given_name' => "Abdug'afforxon"] ,
+            ['student_id' => "211782" ,'surname' => "Ahmedov",'given_name' => "Suhrob"] ,
+            ['student_id' => "220708" ,'surname' => "Azimxo'jayev",'given_name' => "Abdulazizxo'ja"] ,
+            ['student_id' => "T19B0007" ,'surname' => "Botirov",'given_name' => "Saydiaxror"] ,
+            ['student_id' => "214843" ,'surname' => "Boysoatov",'given_name' => "Asilbek"] ,
+        ];
+        foreach ($studentsData as $studentData){
+            Student::create([
+                'student_id' => $studentData['student_id'],
+                'surname' => $studentData['surname'],
+                'given_name' => $studentData['given_name'],
+            ]);
+        }
     }
 }
