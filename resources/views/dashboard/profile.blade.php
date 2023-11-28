c2175f6fb4f308e2777af1412612859deadb0ce2
NodirXakimov <NodirXakimov@yandex.ru>
2023-11-28 11:24:34 +0500

    Camera working on modal

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -8,6 +8,18 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        #video-container {
+            max-width: 100%;
+            margin: 20px auto;
+        }
+
+        #captured-photo {
+            max-width: 100%;
+            height: auto;
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
@@ -45,24 +57,30 @@
 
                 </div>
 
+
                 {{-- Modal for capturing photo section start --}}
-
-
-
-                <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                <div class="modal modal-lg fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
                     tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                                <h5 class="modal-title" id="staticBackdropLabel">Talabani suratga olish</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <div class="container-fluid">
-                                    
-                                    
+                                        <div id="video-container d-flex align-items-center justify-content-center">
+                                            <video id="video" autoplay playsinline></video>
+                                            <button type="button" onclick="capturePhoto()">Capture Photo</button>
+                                        </div>
 
+                                        <img id="captured-photo" alt="Captured Photo">
+                                        <input type="file" id="upload-input" name="file" style="display: none;">
+
+                                        <a id="download-link" style="display: none;">Download Photo</a>
+
+                                        <button type="submit">Upload Photo</button>
                                 </div>
                             </div>
                             <div class="modal-footer">
@@ -72,8 +90,6 @@
                         </div>
                     </div>
                 </div>
-
-
                 {{-- Modal for capturing photo section end --}}
 
                 <div class="col-lg-8">
@@ -118,20 +134,13 @@
                                         <p class="mb-0">Telefon raqami</p>
                                     </div>
                                     <div class="col-sm-8">
-                                            <div class="input-group flex-nowrap">
-                                                <span class="input-group-text" id="addon-wrapping">+998</span>
-                                                <input 
-                                                    type="text" 
-                                                    class="form-control mb-0 text-muted" 
-                                                    id="phone_number"
-                                                    name="phone_number" 
-                                                    value="{{ $student->phone_number }}" 
-                                                    required
-                                                    aria-label="Username" 
-                                                    aria-describedby="addon-wrapping"
-                                                    onkeydown="phoneNumberFormatter()"
-                                                >
-                                            </div>
+                                        <div class="input-group flex-nowrap">
+                                            <span class="input-group-text" id="addon-wrapping">+998</span>
+                                            <input type="text" class="form-control mb-0 text-muted"
+                                                id="phone_number" name="phone_number"
+                                                value="{{ $student->phone_number }}" required aria-label="Username"
+                                                aria-describedby="addon-wrapping" onkeyup="phoneNumberFormatter()">
+                                        </div>
                                     </div>
                                 </div>
                                 <hr>
@@ -140,8 +149,14 @@
                                         <p class="mb-0">Ota-onasining telefon raqami</p>
                                     </div>
                                     <div class="col-sm-8">
-                                        <input type="text" class="form-control mb-0 text-muted"
-                                            name="contact_number" value="{{ $student->contact_number }}">
+                                        <div class="input-group flex-nowrap">
+                                            <span class="input-group-text" id="addon-wrapping">+998</span>
+                                            <input type="text" class="form-control mb-0 text-muted"
+                                                id="parents_phone_number" name="contact_number"
+                                                value="{{ $student->contact_number }}"
+                                                onkeyup="phoneNumberFormatter()">
+                                        </div>
+
                                     </div>
                                 </div>
                                 <hr>
@@ -156,39 +171,103 @@
                         </div>
                     </form>
 
-
                 </div>
             </div>
         </div>
     </section>
 
-    <script>
-        function formatPhoneNumber(value) {
-            if(!value) return value;
-            const phoneNumber = value.replace(/[^\d]/g, '');
-            console.log(phoneNumber.length)
-            const phoneNumberLength = phoneNumber.length;
-            if(phoneNumberLength < 2) return phoneNumber;
-            if(phoneNumberLength < 3) {
-                return `${phoneNumber.slice(0,2)} ${phoneNumber.slice(2)}`;
-            }
-            if(phoneNumberLength < 5) {
-                return `${phoneNumber.slice(0,2)} ${phoneNumber.slice(2, 5,)}`;
-            }
-            if(phoneNumberLength < 7) {
-                return `${phoneNumber.slice(0,2)} ${phoneNumber.slice(2, 5,)}-${phoneNumber.slice(5, 7)}`;
-            }
-            return `${phoneNumber.slice(0,2)} ${phoneNumber.slice(2, 5,)}-${phoneNumber.slice(5, 7)}-${phoneNumber.slice(7, 9)}`;
-
-        }
-        function phoneNumberFormatter() {
-            const inputField = document.getElementById('phone_number')
-            const formattedInputValue = formatPhoneNumber(inputField.value)
-            inputField.value = formattedInputValue;
-        }
-    </script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+
+    <script defer>
+        // PHONE NUMBER FORMATTER STARTS HERE
+        function formatPhoneNumber(value) {
+            if (!value) return value;
+
+            const phoneNumber = value.replace(/[^\d]/g, '');
+            const phoneNumberLength = phoneNumber.length;
+
+            if (phoneNumberLength <= 2) {
+                return phoneNumber;
+            } else if (phoneNumberLength <= 5) {
+                return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2)}`;
+            } else if (phoneNumberLength <= 7) {
+                return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)}-${phoneNumber.slice(5)}`;
+            } else {
+                return `${phoneNumber.slice(0, 2)} ${phoneNumber.slice(2, 5)}-${phoneNumber.slice(5, 7)}-${phoneNumber.slice(7, 9)}`;
+            }
+        }
+
+        function phoneNumberFormatter() {
+            const inputField = document.getElementById(event.target.id)
+            const formattedInputValue = formatPhoneNumber(inputField.value)
+            inputField.value = formattedInputValue;
+            console.log(formattedInputValue);
+        }
+        // PHONE NUMBER FORMATTER ENDS HERE
+
+
+        // CAMERA CAPTURING SECTION START
+        console.log("Script is running");
+        const video = document.getElementById('video');
+        console.log(video);
+        const capturedPhoto = document.getElementById('captured-photo');
+        console.log(capturePhoto);
+        const uploadInput = document.getElementById('upload-input');
+        console.log(uploadInput);
+        let stream;
+
+        const constraints = {
+            video: {
+                facingMode: 'user'
+            }
+        };
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((cameraStream) => {
+                video.srcObject = cameraStream;
+                stream = cameraStream;
+            })
+            .catch((error) => {
+                console.error('Error accessing the camera:', error);
+            });
+
+        function capturePhoto() {
+            if (stream) {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+
+                // Set the canvas dimensions to the video stream dimensions
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+
+                // Draw the current frame from the video stream onto the canvas
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                // Convert the canvas content to a data URL representing the image
+                const dataUrl = canvas.toDataURL('image/png');
+
+                // Display the captured photo
+                capturedPhoto.src = dataUrl;
+                capturedPhoto.style.display = 'block';
+
+                // Display and enable the download link
+                downloadLink.href = dataUrl;
+                downloadLink.style.display = 'inline';
+                downloadLink.download = 'captured_photo.png'; // specify the filename for download
+
+                // Set the value of the upload input to the data URL
+                uploadInput.value = dataUrl;
+
+                // Optionally, you can save the data URL or perform other actions here
+                // For example, you can send the dataUrl to a server for further processing
+            } else {
+                alert('Camera stream not available. Make sure the camera is accessible.');
+            }
+        }
+        // CAMERA CAPTURING SECTION END
     </script>
 </body>
 
