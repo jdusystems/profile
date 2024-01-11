ea441fef6cbec655ba285ab9d1ea13504e8bb939
Jakhongir1907 <151619643+Jakhongir1907@users.noreply.github.com>
2024-01-11 14:07:30 +0500

    Changes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <form method="post" action="{{ route('talaba.top') }}">
+    <form method="post" action="{{ route('students.store') }}">
         @csrf
         <div class="container d-flex flex-column gap-2 align-items-center justify-content-center min-vh-100">
             <div style="width: 300px">
@@ -23,8 +23,6 @@
             </div>
         </div>
     </form>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
